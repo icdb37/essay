325f8f24d9d1a24ad64edabe468d2d2f60852601
icdb37 <icdb37.163.com>
2024-12-16 16:54:59 +0800

    随记项目初始化

---
+++ public/searchindex.zh.js
@@ -0,0 +1,26 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "随记",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "类别",
+    "uri": "/zh/categories/index.html"
+  },
+  {
+    "breadcrumb": "随记",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "标签",
+    "uri": "/zh/tags/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "随记",
+    "uri": "/zh/index.html"
+  }
+]
